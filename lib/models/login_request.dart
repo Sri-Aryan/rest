2c4fba29c229710c28f203927cd808e7c3c12810
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-06 18:58:56 +0530

    login model

---
+++ lib/models/login_request.dart
@@ -0,0 +1,28 @@
+class LoginRequest {
+  final String email;
+  final String password;
+  final String role;
+  final String deviceToken;
+  final String type;
+  final String? socialId;
+
+  LoginRequest({
+    required this.email,
+    required this.password,
+    required this.role,
+    required this.deviceToken,
+    required this.type,
+    this.socialId,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+      'role': "farmer",
+      'device_token': "0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx",
+      'type': type,
+      if (socialId != null) 'social_id': "0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx",
+    };
+  }
+}
