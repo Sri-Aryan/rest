a77f7a66283101bbed210a0b24685a508400481a
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-06 18:59:26 +0530

    register model

---
+++ lib/models/register_request.dart
@@ -0,0 +1,90 @@
+class BusinessHours {
+  final List<String> mon;
+  final List<String> tue;
+  final List<String> wed;
+  final List<String> thu;
+  final List<String> fri;
+  final List<String> sat;
+  final List<String> sun;
+
+  BusinessHours({
+    required this.mon,
+    required this.tue,
+    required this.wed,
+    required this.thu,
+    required this.fri,
+    required this.sat,
+    required this.sun,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'mon': mon,
+      'tue': tue,
+      'wed': wed,
+      'thu': thu,
+      'fri': fri,
+      'sat': sat,
+      'sun': sun,
+    };
+  }
+}
+
+class RegisterRequest {
+  final String fullName;
+  final String email;
+  final String phone;
+  final String password;
+  final String role;
+  final String businessName;
+  final String informalName;
+  final String address;
+  final String city;
+  final String state;
+  final int zipCode;
+  final String? registrationProof;
+  final BusinessHours businessHours;
+  final String deviceToken;
+  final String type;
+  final String? socialId;
+
+  RegisterRequest({
+    required this.fullName,
+    required this.email,
+    required this.phone,
+    required this.password,
+    required this.role,
+    required this.businessName,
+    required this.informalName,
+    required this.address,
+    required this.city,
+    required this.state,
+    required this.zipCode,
+    this.registrationProof,
+    required this.businessHours,
+    required this.deviceToken,
+    required this.type,
+    this.socialId,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'full_name': fullName,
+      'email': email,
+      'phone': phone,
+      'password': password,
+      'role': role,
+      'business_name': businessName,
+      'informal_name': informalName,
+      'address': address,
+      'city': city,
+      'state': state,
+      'zip_code': zipCode,
+      if (registrationProof != null) 'registration_proof': registrationProof,
+      'business_hours': businessHours.toJson(),
+      'device_token': deviceToken,
+      'type': type,
+      if (socialId != null) 'social_id': socialId,
+    };
+  }
+}
