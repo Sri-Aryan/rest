d5add44e70ed727ce9dcdea790ea433daf1aa7e7
Aryan Srivastava <143885879+Sri-Aryan@users.noreply.github.com>
2026-03-07 18:45:12 +0530

    api model update

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,17 +1,36 @@
 class ApiResponse {
-  final String success;
-  final String message;
-  final String? token;
-  final bool? isVerified;
+  final dynamic success;
+  final String? message;
+  final dynamic data;
 
-  ApiResponse({required this.success, required this.message, this.token, this.isVerified});
+  ApiResponse({
+    required this.success,
+    this.message,
+    this.data,
+  });
 
   factory ApiResponse.fromJson(Map<String, dynamic> json) {
     return ApiResponse(
       success: json['success'],
       message: json['message'],
-      token: json['token'],
-      isVerified: json['isVerified'] == 'true',
+      data: json['data'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'success': success,
+      'message': message,
+      'data': data,
+    };
+  }
+
+  bool get isSuccess {
+    if (success is String) {
+      return success == 'true';
+    } else if (success is bool) {
+      return success as bool;
+    }
+    return false;
   }
 }
